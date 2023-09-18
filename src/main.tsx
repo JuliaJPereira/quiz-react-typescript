0bdc8fb364304619726c051060b6747bbcc2c92c
JuliaJPereira <juliapereira.odc@gmail.com>
2023-09-18 18:10:32 -0300

    Rename quiz-page

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Quiz from './quiz.tsx'
+import Quiz from './quiz-page.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
