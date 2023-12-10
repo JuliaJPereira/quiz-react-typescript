5b82d8643063b9500f5fce705c9bc8f8e46bb552
JuliaJPereira <juliapereira.odc@gmail.com>
2023-12-09 21:09:24 -0300

    adicionar botão que leva para escolher quiz

--- src/quiz-finished.tsx
+++ src/quiz-finished.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export function QuizFinished() {
+  const navigate = useNavigate();
+
+  const handleClickSelectQuiz = () => {
+    navigate("/selecione-quiz");
+  };
+
   return (
     <div>
       <h1>Quiz Finalizado</h1>
+      <button onClick={handleClickSelectQuiz}>Jogar novamente</button>
     </div>
   );
 }
